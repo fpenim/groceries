0ed2ce60f2f60ba34497c9aacc8d2be01b4b923f
Flávia Penim <flaviapenim@gmail.com>
2020-06-18 14:50:50 +0100

    Initial commit.

---
+++ src/main/kotlin/com/swarmbit/inmypantry/InmypantryApplication.kt
@@ -0,0 +1,11 @@
+package com.swarmbit.inmypantry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InmypantryApplication
+
+fun main(args: Array<String>) {
+	runApplication<InmypantryApplication>(*args)
+}
