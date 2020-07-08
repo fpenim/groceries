54008c7f906ecad07fa39a378f3829399623f078
Flávia Penim <flaviapenim@gmail.com>
2020-07-08 14:56:55 +0100

    Added some KotlinDocs. Removed obsolete custom exceptions.

--- src/main/kotlin/com/fpenim/groceries/model/Quantity.kt
+++ src/main/kotlin/com/fpenim/groceries/model/Quantity.kt
@@ -6,9 +6,18 @@ data class Quantity(val value: Double, val unit: MetricUnit) {
         return "$value ${unit.symbol}"
     }
 
+    /**
+     * Compatibility is based on units being convertible between each other.
+     *
+     * - Litre and millilitre are both Volume metrics - they are compatible.
+     * - Kilogram is a Mass metric and decilitre is a Volume metric - they are incompatible.
+     */
     fun isCompatibleWith(quantities: List<Quantity>): Boolean = quantities.all { this.unit::class.java == it.unit::class.java }
 }
 
+/**
+ * TODO
+ */
 fun MutableList<Quantity>.sum(): Quantity {
 
     if (this.isEmpty()) {
